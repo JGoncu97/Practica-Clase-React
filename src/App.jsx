6dbde2b425e5dbe64b699972775aa551577204ea
JGoncu97 <jggc1197@gmail.com>
2024-10-08 10:11:28 -0500

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 
-import './App.css'
 import {HeaderPage} from './Components/Header/Header'
 
 
-const App = () => {
+export const App = () => {
 
 
   return (
     <>
-      <HeaderPage>
+      <HeaderPage/>
         <h3>Hellow World!!!</h3>
-      </HeaderPage>
+      
     </>
   )
 }
 
-export default App
